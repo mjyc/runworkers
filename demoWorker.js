7565d1985bca8d4e3ff17b6ccf4cc0fa1aa94854
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2020-12-12 22:25:17 -0800

    Update demoWorker to return more meaningful result

--- demoWorker.js
+++ demoWorker.js
@@ -2,5 +2,5 @@ const { workerData, parentPort } = require("worker_threads");
 
 console.log("workerData", workerData);
 setTimeout(() => {
-  parentPort.postMessage(Object.assign(workerData, { done: true }));
+  parentPort.postMessage(Object.assign(workerData, { stamp: Date.now() }));
 }, workerData.sleep);
